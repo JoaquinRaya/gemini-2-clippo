237ef596016dc79694c2931957e59de66e9b93c1
JoaquinRaya <joaquin.raya@gmail.com>
2025-03-31 11:20:05 +0000

    Moved all the connection activities to screen capture service, but this won't work for real. Need to hook up receive/send events from app component and control tray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-import { MultimodalLiveService } from '../gemini/gemini-client.service';
-import { Subscription } from 'rxjs';
+// import { MultimodalLiveService } from '../gemini/gemini-client.service';
+// import { Subscription } from 'rxjs';
 import { Part } from '@google/generative-ai';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { LiveConfig, ModelTurn, TurnComplete } from '../gemini/types';
+// import { LiveConfig, ModelTurn, TurnComplete } from '../gemini/types';
 import { ControlTrayComponent } from './control-tray/control-tray.component';
+import { ChatMessage } from '../gemini/screen-capture.service'
 
-type ChatMessage = {
-  role: string;
-  text: string;
-}
+// type ChatMessage = {
+//   role: string;
+//   text: string;
+// }
 
 @Component({
   selector: 'app-root',
@@ -18,16 +19,17 @@ type ChatMessage = {
   styleUrls: ['./app.component.css'],
   imports: [CommonModule, ReactiveFormsModule, ControlTrayComponent],
 })
-export class AppComponent implements OnInit, OnDestroy {
+// export class AppComponent implements OnInit, OnDestroy {
+  export class AppComponent {
   @ViewChild('myVideo') myVideoRef!: ElementRef<HTMLVideoElement>;
 
   title = 'gemini-2-live-angular';
   isConnected: boolean = false;
-  volume: number = 0;
-  streamedMessage: string = '';
+  // volume: number = 0;
+  // streamedMessage: string = '';
   messages: ChatMessage[] = [];
-  private connectedSubscription: Subscription | undefined;
-  private contentSubscription: Subscription | undefined;
+  // private connectedSubscription: Subscription | undefined;
+  // private contentSubscription: Subscription | undefined;
 
 
   chatForm = new FormGroup({
@@ -35,94 +37,97 @@ export class AppComponent implements OnInit, OnDestroy {
   });
   isFormEmpty: boolean = this.chatForm.value?.message?.length === 0;
 
+  send() :void{
 
-  constructor(private multimodalLiveService: MultimodalLiveService) { }
-
-  ngOnInit(): void {
-    this.connectedSubscription = this.multimodalLiveService.connected$.subscribe(
-      (connected) => {
-        console.log('Connected:', connected);
-        this.isConnected = connected;
-      },
-    );
-    this.contentSubscription = this.multimodalLiveService.content$.subscribe(
-      (data) => {
-        if (!data) return;
-        let turn = data as ModelTurn;
-        let turnComplete = (data as TurnComplete).turnComplete;
-        if (turn) {
-          if (this.streamedMessage.length > 0) {
-            this.messages.pop();
-          } 
-          let incomingMessage = turn.modelTurn.parts?.[0]?.text as string;
-          if (incomingMessage) {
-            this.streamedMessage += incomingMessage;
-            this.messages.push({
-              role: 'model',
-              text: this.streamedMessage
-            });
-          }
-        }
-        if (turnComplete) {
-          this.messages.push({
-            role: 'model',
-            text: this.streamedMessage
-          });
-          this.streamedMessage = '';
-        }
-      },
-    );
   }
 
-  ngOnDestroy(): void {
-    if (this.connectedSubscription) {
-      this.connectedSubscription.unsubscribe();
-      console.log('Connected:', this.isConnected);
-    }
-  }
+  // constructor(private multimodalLiveService: MultimodalLiveService) { }
 
-  connect(): void {
-    let config : LiveConfig = {
-      model: "models/gemini-2.0-flash-exp",
-      generationConfig: {
-        // responseModalities: "text",
-        responseModalities: "audio", // note "audio" doesn't send a text response over
-        speechConfig: {
-          voiceConfig: { prebuiltVoiceConfig: { voiceName: "Aoede" } },
-        },
-      },
-      systemInstruction: {
-        parts: [
-          {
-            text: 'You are a helpful assistant.',
-          },
-        ],
-      },
-    };
+  // ngOnInit(): void {
+  //   this.connectedSubscription = this.multimodalLiveService.connected$.subscribe(
+  //     (connected) => {
+  //       console.log('Connected:', connected);
+  //       this.isConnected = connected;
+  //     },
+  //   );
+  //   this.contentSubscription = this.multimodalLiveService.content$.subscribe(
+  //     (data) => {
+  //       if (!data) return;
+  //       let turn = data as ModelTurn;
+  //       let turnComplete = (data as TurnComplete).turnComplete;
+  //       if (turn) {
+  //         if (this.streamedMessage.length > 0) {
+  //           this.messages.pop();
+  //         }
+  //         let incomingMessage = turn.modelTurn.parts?.[0]?.text as string;
+  //         if (incomingMessage) {
+  //           this.streamedMessage += incomingMessage;
+  //           this.messages.push({
+  //             role: 'model',
+  //             text: this.streamedMessage
+  //           });
+  //         }
+  //       }
+  //       if (turnComplete) {
+  //         this.messages.push({
+  //           role: 'model',
+  //           text: this.streamedMessage
+  //         });
+  //         this.streamedMessage = '';
+  //       }
+  //     },
+  //   );
+  // }
 
-    this.multimodalLiveService.connect(config).catch(err => {
-      console.error("Failed to connect:", err);
-    });
-  }
+  // ngOnDestroy(): void {
+  //   if (this.connectedSubscription) {
+  //     this.connectedSubscription.unsubscribe();
+  //     console.log('Connected:', this.isConnected);
+  //   }
+  // }
 
-  disconnect(): void {
-    this.multimodalLiveService.disconnect();
-  }
+  // connect(): void {
+  //   let config : LiveConfig = {
+  //     model: "models/gemini-2.0-flash-exp",
+  //     generationConfig: {
+  //       // responseModalities: "text",
+  //       responseModalities: "audio", // note "audio" doesn't send a text response over
+  //       speechConfig: {
+  //         voiceConfig: { prebuiltVoiceConfig: { voiceName: "Aoede" } },
+  //       },
+  //     },
+  //     systemInstruction: {
+  //       parts: [
+  //         {
+  //           text: 'You are a helpful assistant.',
+  //         },
+  //       ],
+  //     },
+  //   };
 
-  send(): void {
-    this.streamedMessage = ''; // reset streamed message
-    let message = (this.chatForm.value?.message as string)?.trim();
-    if (!message) return;
-    let part: Part | Part[] = {
-      text: message,
-    };
-    this.multimodalLiveService.send(part);
-    this.messages.push({
-      role: 'user',
-      text: message
-    });
-    this.chatForm.reset();
-  }
+  //   this.multimodalLiveService.connect(config).catch(err => {
+  //     console.error("Failed to connect:", err);
+  //   });
+  // }
+
+  // disconnect(): void {
+  //   this.multimodalLiveService.disconnect();
+  // }
+
+  // send(): void {
+  //   this.streamedMessage = ''; // reset streamed message
+  //   let message = (this.chatForm.value?.message as string)?.trim();
+  //   if (!message) return;
+  //   let part: Part | Part[] = {
+  //     text: message,
+  //   };
+  //   this.multimodalLiveService.send(part);
+  //   this.messages.push({
+  //     role: 'user',
+  //     text: message
+  //   });
+  //   this.chatForm.reset();
+  // }
 
   reset(): void {
     this.chatForm.reset();
--- src/gemini/screen-capture.service.ts
+++ src/gemini/screen-capture.service.ts
@@ -1,5 +1,8 @@
-import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject, Subscription, takeUntil } from 'rxjs';
+import { MultimodalLiveService } from '../gemini/gemini-client.service';
+import { LiveConfig, ModelTurn, TurnComplete } from '../gemini/types';
+import { Part } from '@google/generative-ai';
 
 interface UseMediaStreamResult {
     isStreaming: Observable<boolean>;
@@ -7,17 +10,29 @@ interface UseMediaStreamResult {
     stop: () => void;
 }
 
+export type ChatMessage = {
+    role: string;
+    text: string;
+  }
+
 @Injectable({
     providedIn: 'root'
 })
-export class ScreenCaptureService implements OnDestroy {
+export class ScreenCaptureService implements OnInit, OnDestroy {
     private streamSubject = new BehaviorSubject<MediaStream | null>(null);
     stream$ = this.streamSubject.asObservable();
     private isStreamingSubject = new BehaviorSubject<boolean>(false);
     isStreaming$ = this.isStreamingSubject.asObservable();
     private ngUnsubscribe = new Subject<void>();
 
-    constructor() {
+    isConnected: boolean = false;
+    volume: number = 0;
+    streamedMessage: string = '';
+    messages: ChatMessage[] = [];
+    private connectedSubscription: Subscription | undefined;
+    private contentSubscription: Subscription | undefined;
+
+    constructor(private multimodalLiveService: MultimodalLiveService) {
         this.stream$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(stream => {
             if (stream) {
                 const handleStreamEnded = () => {
@@ -37,11 +52,52 @@ export class ScreenCaptureService implements OnDestroy {
             }
         });
     }
+    ngOnInit(): void {
+        this.connectedSubscription = this.multimodalLiveService.connected$.subscribe(
+          (connected) => {
+            console.log('Connected:', connected);
+            this.isConnected = connected;
+          },
+        );
+        this.contentSubscription = this.multimodalLiveService.content$.subscribe(
+          (data) => {
+            if (!data) return;
+            let turn = data as ModelTurn;
+            let turnComplete = (data as TurnComplete).turnComplete;
+            if (turn) {
+              if (this.streamedMessage.length > 0) {
+                this.messages.pop();
+              }
+              let incomingMessage = turn.modelTurn.parts?.[0]?.text as string;
+              if (incomingMessage) {
+                this.streamedMessage += incomingMessage;
+                this.messages.push({
+                  role: 'model',
+                  text: this.streamedMessage
+                });
+              }
+            }
+            if (turnComplete) {
+              this.messages.push({
+                role: 'model',
+                text: this.streamedMessage
+              });
+              this.streamedMessage = '';
+            }
+          },
+        );
+      }
+    
 
     ngOnDestroy(): void {
         this.ngUnsubscribe.next();
         this.ngUnsubscribe.complete();
         this.stop();
+
+        if (this.connectedSubscription) {
+            this.connectedSubscription.unsubscribe();
+            console.log('Connected:', this.isConnected);
+          }
     }
 
     get isStreaming() {
@@ -79,4 +135,47 @@ export class ScreenCaptureService implements OnDestroy {
             this.isStreamingSubject.next(false);
         }
     }
+
+    connect(): void {
+        let config : LiveConfig = {
+          model: "models/gemini-2.0-flash-exp",
+          generationConfig: {
+            // responseModalities: "text",
+            responseModalities: "audio", // note "audio" doesn't send a text response over
+            speechConfig: {
+              voiceConfig: { prebuiltVoiceConfig: { voiceName: "Aoede" } },
+            },
+          },
+          systemInstruction: {
+            parts: [
+              {
+                text: 'You are a helpful assistant.',
+              },
+            ],
+          },
+        };
+    
+        this.multimodalLiveService.connect(config).catch(err => {
+          console.error("Failed to connect:", err);
+        });
+      }
+    
+      disconnect(): void {
+        this.multimodalLiveService.disconnect();
+      }
+      // send(): void {
+      send(message: string): void {
+        this.streamedMessage = ''; // reset streamed message
+        // let message = (this.chatForm.value?.message as string)?.trim();
+        if (!message) return;
+        let part: Part | Part[] = {
+          text: message,
+        };
+        this.multimodalLiveService.send(part);
+        this.messages.push({
+          role: 'user',
+          text: message
+        });
+        // this.chatForm.reset();
+      }
 }
